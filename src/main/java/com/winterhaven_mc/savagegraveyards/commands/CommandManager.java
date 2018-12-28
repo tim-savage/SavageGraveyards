2db68d3e087fbde0b3947375034d49cad6ee73db
Tim Savage <tim.savage@gmail.com>
2018-12-27 21:36:46 -0600

    simplify imports

--- src/main/java/com/winterhaven_mc/savagegraveyards/commands/CommandManager.java
+++ src/main/java/com/winterhaven_mc/savagegraveyards/commands/CommandManager.java
@@ -15,10 +15,7 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 /**
