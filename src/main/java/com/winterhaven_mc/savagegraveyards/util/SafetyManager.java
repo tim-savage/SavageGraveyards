c5b3178dabac6ad5c04898abdb2091feefe4cab2
Tim Savage <tim.savage@gmail.com>
2019-01-06 20:30:01 -0600

    revert to safety map, using safety task as value; cancel task if player dies again before timeout, to prevent early safety timeout

--- src/main/java/com/winterhaven_mc/savagegraveyards/util/SafetyManager.java
+++ src/main/java/com/winterhaven_mc/savagegraveyards/util/SafetyManager.java
@@ -5,8 +5,9 @@ import com.winterhaven_mc.savagegraveyards.messages.MessageId;
 
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
-import java.util.Set;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -19,8 +20,8 @@ public class SafetyManager {
 	// reference to main class
 	private final PluginMain plugin;
 
-	// safety cooldown set
-	private Set<UUID> safetyCooldownSet;
+	// safety cooldown map
+	private Map<UUID, BukkitTask> safetyCooldownMap;
 
 
 	/**
@@ -33,8 +34,8 @@ public class SafetyManager {
 		// set reference to main class
 		this.plugin = plugin;
 
-		// instantiate safety cooldown set
-		safetyCooldownSet = ConcurrentHashMap.newKeySet();
+		// instantiate safety cooldown map
+		safetyCooldownMap = new ConcurrentHashMap<>();
 	}
 
 
@@ -46,52 +47,57 @@ public class SafetyManager {
 	 */
 	public void putPlayer(final Player player, int duration) {
 
+		// if duration is zero, do nothing and return
+		if (duration == 0) {
+			return;
+		}
+
 		// if duration is negative, use configured default
 		if (duration < 0) {
 			duration = plugin.getConfig().getInt("safety-time");
 		}
 
-		// if duration is less than one, do nothing and return
-		if (duration < 1) {
-			return;
-		}
-
-		// add player to safety cooldown set
-		safetyCooldownSet.add(player.getUniqueId());
-
 		// send safety message to player
 		plugin.messageManager.sendMessage(player, MessageId.SAFETY_COOLDOWN_START, duration);
 
 		// create task to remove player from map after duration
-		new BukkitRunnable() {
+		BukkitTask task = new BukkitRunnable() {
 			@Override
 			public void run() {
 				removePlayer(player);
 				plugin.messageManager.sendMessage(player, MessageId.SAFETY_COOLDOWN_END);
 			}
 		}.runTaskLater(plugin, duration * 20);
+
+		// if player is already in cooldown map, cancel existing task
+		if (isPlayerProtected(player)) {
+			safetyCooldownMap.get(player.getUniqueId()).cancel();
+		}
+
+		// add player to safety cooldown map
+		safetyCooldownMap.put(player.getUniqueId(), task);
 	}
 
 
 	/**
-	 * Remove player from safety cooldown set
+	 * Remove player from safety cooldown map
 	 *
-	 * @param player the player to be removed from the safety cooldown set
+	 * @param player the player to be removed from the safety cooldown map
 	 */
 	private void removePlayer(final Player player) {
-		safetyCooldownSet.remove(player.getUniqueId());
+		safetyCooldownMap.remove(player.getUniqueId());
 	}
 
 
 	/**
-	 * Check if player is in safety cooldown set
+	 * Check if player is in safety cooldown map
 	 *
-	 * @param player the player to test if in the safety cooldown set
-	 * @return {@code true} if the player is in the safety cooldown set, {@code false} if they are not
+	 * @param player the player to test if in the safety cooldown map
+	 * @return {@code true} if the player is in the safety cooldown map, {@code false} if they are not
 	 */
 	public boolean isPlayerProtected(final Player player) {
 
-		return safetyCooldownSet.contains(player.getUniqueId());
+		return safetyCooldownMap.containsKey(player.getUniqueId());
 	}
 
 }
