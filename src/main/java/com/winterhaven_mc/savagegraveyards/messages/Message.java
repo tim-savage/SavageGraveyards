0d379beaaed2d347cd4209f08c4d6137cf939e2b
Tim Savage <tim.savage@gmail.com>
2020-07-12 19:02:39 -0500

    add javadocs

--- src/main/java/com/winterhaven_mc/savagegraveyards/messages/Message.java
+++ src/main/java/com/winterhaven_mc/savagegraveyards/messages/Message.java
@@ -6,10 +6,25 @@ import org.bukkit.command.CommandSender;
 
 public class Message extends AbstractMessage<MessageId,Macro> {
 
+
+	/**
+	 * Private class constructor; calls inherited super class constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 */
 	public Message(CommandSender recipient, MessageId messageId) {
 		super(recipient, messageId);
 	}
 
+
+	/**
+	 * Static class constructor
+	 *
+	 * @param recipient the message recipient
+	 * @param messageId the enum entry representing the message to be displayed
+	 * @return new instance of Message created with private constructor
+	 */
 	public static Message create(CommandSender recipient, MessageId messageId) {
 		return new Message(recipient, messageId);
 	}
