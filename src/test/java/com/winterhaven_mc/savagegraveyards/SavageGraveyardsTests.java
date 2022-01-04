c457ead0d85294a3a1ab9489403fea5e45bb062c
Tim Savage <tim.savage@gmail.com>
2022-01-04 02:02:57 -0600

    remove play sound tests

--- src/test/java/com/winterhaven_mc/savagegraveyards/SavageGraveyardsTests.java
+++ src/test/java/com/winterhaven_mc/savagegraveyards/SavageGraveyardsTests.java
@@ -2,29 +2,39 @@ package com.winterhaven_mc.savagegraveyards;
 
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
+import com.winterhaven_mc.savagegraveyards.sounds.SoundId;
 import org.bukkit.configuration.Configuration;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class PluginMainTests {
+public class SavageGraveyardsTests {
 
     private ServerMock server;
     private PluginMain plugin;
 
+
     @BeforeAll
     public void setUp() {
+
         // Start the mock server
         server = MockBukkit.mock();
 
         // start the mock plugin
         plugin = MockBukkit.load(PluginMain.class);
+
+        // create mock world
+//        WorldMock world = MockBukkit.getMock().addSimpleWorld("world");
+
+        // create mock player
+//        PlayerMock player = server.addPlayer("testy");
     }
 
     @AfterAll
@@ -54,6 +64,7 @@ public class PluginMainTests {
         }
     }
 
+
     @Nested
     @DisplayName("Test plugin main objects.")
     class PluginTests {
@@ -75,6 +86,7 @@ public class PluginMainTests {
             Assertions.assertNotNull(plugin.soundConfig);
         }
     }
+
 
     @Nested
     @DisplayName("Test plugin config.")
@@ -99,7 +111,8 @@ public class PluginMainTests {
         @Test
         @DisplayName("test configured language.")
         void GetLanguage() {
-            Assertions.assertEquals("en-US", config.getString("language"));
+            Assertions.assertEquals("en-US", config.getString("language"),
+                    "configured language does not match en-US");
         }
 
         @SuppressWarnings("unused")
@@ -121,6 +134,60 @@ public class PluginMainTests {
         @DisplayName("ConfigSetting enum matches config file key/value pairs.")
         void ConfigFileKeysContainsEnumKey(ConfigSetting configSetting) {
             Assertions.assertEquals(configSetting.getValue(), plugin.getConfig().getString(configSetting.getKey()));
+        }
+    }
+
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    @DisplayName("Test Sounds config.")
+    class SoundTests {
+
+        // collection of enum sound name strings
+        Collection<String> enumSoundNames = new HashSet<>();
+
+        // class constructor
+        SoundTests() {
+            // add all SoundId enum values to collection
+            for (com.winterhaven_mc.savagegraveyards.sounds.SoundId SoundId : SoundId.values()) {
+                enumSoundNames.add(SoundId.name());
+            }
+        }
+
+        @Test
+        @DisplayName("Sounds config is not null.")
+        void SoundConfigNotNull() {
+            Assertions.assertNotNull(plugin.soundConfig);
+        }
+
+        @SuppressWarnings("unused")
+        Collection<String> GetConfigFileKeys() {
+            return plugin.soundConfig.getSoundConfigKeys();
+        }
+
+        @ParameterizedTest
+        @EnumSource(SoundId.class)
+        @DisplayName("enum member soundId is contained in getConfig() keys.")
+        void FileKeysContainsEnumValue(SoundId soundId) {
+            Assertions.assertTrue(plugin.soundConfig.isValidSoundConfigKey(soundId.name()),
+                    "Enum value '" + soundId.name() + "' does not have matching key in sounds.yml.");
+        }
+
+        @ParameterizedTest
+        @MethodSource("GetConfigFileKeys")
+        @DisplayName("config file key has matching key in enum sound names")
+        void SoundConfigEnumContainsAllFileSounds(String key) {
+            Assertions.assertTrue(enumSoundNames.contains(key),
+                    "File key does not have matching key in enum sound names.");
+        }
+
+        @ParameterizedTest
+        @MethodSource("GetConfigFileKeys")
+        @DisplayName("sound file key has valid bukkit sound name")
+        void SoundConfigFileHasValidBukkitSound(String key) {
+            String bukkitSoundName = plugin.soundConfig.getBukkitSoundName(key);
+            Assertions.assertTrue(plugin.soundConfig.isValidBukkitSoundName(bukkitSoundName),
+                    "File key '" + key + "' has invalid bukkit sound name: " + bukkitSoundName);
         }
     }
 
